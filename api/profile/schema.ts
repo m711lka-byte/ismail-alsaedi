e96886d80fe25bccf60559b36b0365b2af28cbc5
m711lka-byte <m711lka@gmail.com>
2026-08-13 07:09:54 +0300

    Add files via upload

---
+++ api/profile/schema.ts
@@ -0,0 +1,10 @@
+import { generatePersonKnowledgeGraph } from '../../src/lib/seo';
+
+export default function handler(req: any, res: any) {
+  res.setHeader('Content-Type', 'application/ld+json; charset=UTF-8');
+  res.setHeader('Cache-Control', 'public, max-age=3600, s-maxage=3600');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  
+  const graph = generatePersonKnowledgeGraph();
+  res.status(200).json(graph);
+}
